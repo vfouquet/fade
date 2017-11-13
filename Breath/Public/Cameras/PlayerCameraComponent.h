f4ca6862118582616a351265abcdf411ee3ff91d
v.fouquet <v.fouquet@student.isartdigital.com>
2017-11-13 10:21:02 +0100

    Add PlayerCameraComponent
    [git-p4: depot-paths = "//breath/Breath/Source/": change = 3352]

---
+++ Breath/Public/Cameras/PlayerCameraComponent.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Camera/CameraComponent.h"
+#include "PlayerCameraComponent.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class BREATH_API UPlayerCameraComponent : public UCameraComponent
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
